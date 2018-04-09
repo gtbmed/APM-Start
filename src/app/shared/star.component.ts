67103a4c19bcfd070632cd5ebd85c347b04cc096
John Barson <jkbarson@gmail.com>
2018-04-09 14:44:42 -0400

    use this.ratingClicked.emit in onClick method. This creates and raises an event to the container and gives it the desired message

--- src/app/shared/star.component.ts
+++ src/app/shared/star.component.ts
@@ -17,6 +17,6 @@ export class StarComponent implements OnChanges{
     }
 
     onClick(): void {
-        console.log(`The rating ${this.rating} was clicked!`);
+        this.ratingClicked.emit(`The rating ${this.rating} was clicked!`);
     }
 }
