fbcbfcc8533253e9047f480ac6ce27e01b14d59f
John Barson <jkbarson@gmail.com>
2018-04-18 09:18:28 -0400

    add getProducts method typing it with IProduct.  import the IProduct from the specified folder

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,6 +1,10 @@
 import { Injectable } from "@angular/core";
+import { IProduct } from "./product";
 
 @Injectable
 export class ProductService {
 
+    getProducts(): IProduct {
+        return;
+    }
 }
