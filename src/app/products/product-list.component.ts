ea41bf5ee4cd311bb7f08683fc2bf96e787e7fff
John Barson <jkbarson@gmail.com>
2018-04-18 14:00:19 -0400

    remove hard coded products from product-list component

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -23,28 +23,7 @@ export class ProductListComponent implements OnInit {
     }
     
     filteredProducts: IProduct []; // If we used 'products' to filter, we'd have to get the data from the source again after each filtering
-    products: IProduct[] = [
-        {
-            "productId": 1,
-            "productName": "Leaf Rake",
-            "productCode": "GDN-0011",
-            "releaseDate": "March 19, 2016",
-            "description": "Leaf rake with 48-inch wooden handle.",
-            "price": 19.95,
-            "starRating": 3.2,
-            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
-        },
-        {
-            "productId": 2,
-            "productName": "Garden Cart",
-            "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2016",
-            "description": "15 gallon capacity rolling garden cart",
-            "price": 32.99,
-            "starRating": 4.2,
-            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
-        }
-    ];
+    products: IProduct[] = {}];
 
     constructor(private _productService: ProductService) { // set defaults for the page
         this.filteredProducts = this.products;
