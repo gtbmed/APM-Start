e560c00a6ed95713e15604d3d91515ab0106fcb9
John Barson <jkbarson@gmail.com>
2018-04-30 14:22:27 -0400

    add shell of product-detail component using cli.  This is done for navigating and routing portion of the tutorial

---
+++ src/app/products/product-detail.component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pm-product-detail.component',
+  templateUrl: './product-detail.component.component.html',
+  styleUrls: ['./product-detail.component.component.css']
+})
+export class ProductDetail.ComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
