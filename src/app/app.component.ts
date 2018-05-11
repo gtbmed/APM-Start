419449f3e83e69c4f8b89f3b309a51ec0c78baca
John Barson <jkbarson@gmail.com>
2018-05-11 15:04:01 -0400

    use routerLink directive to tie a route the home and products list menu options

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,8 +9,8 @@ import { ProductService } from "./products/product.service";
         <div class='container-fluid'>
           <a class='navbar-brand'>{{pageTitle}}</a>
           <ul class='nav navbar-nav'>
-            <li><a>Home</a></li>
-            <li><a>Product List</a></li>
+            <li><a [routerLink]="['/welcome']">Home</a></li>
+            <li><a [routerLink]="['/products']">Product List</a></li>
           </ul>
         </div>
       </nav>
