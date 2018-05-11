fd219cecd09271f513df6e8f1606423ea212859f
John Barson <jkbarson@gmail.com>
2018-05-11 14:54:52 -0400

    add new htmlfor navbar to app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,19 @@ import { ProductService } from "./products/product.service";
 
 @Component ({
   selector: 'pm-root',
-  template: ``,
+  template: `
+    <div>
+      <nav class='navbar navbar-default'>
+        <div class='container-fluid'>
+          <a class='navbar-brand'>{{pageTitle}}</a>
+          <ul class='nav navbar-nav'>
+            <li><a>Home</a></li>
+            <li><a>Product List</a></li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+    `,
   providers: [ProductService]
 })
 export class AppComponent {
