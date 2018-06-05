98131425e116242cbdca17a34d77aacb12d55648
John Barson <jkbarson@gmail.com>
2018-06-05 12:35:56 -0400

    add ActivatedRouteSnapshot to canActivate method

--- src/app/products/product-guard.service.ts
+++ src/app/products/product-guard.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import { CanActivate } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot } from '@angular/router';
 
 @Injectable()
 export class ProductGuardService implements CanActivate {
 
   constructor() { }
 
-  canActivate(): boolean {
+  canActivate(route: ActivatedRouteSnapshot): boolean {
     return true;
   }
 
