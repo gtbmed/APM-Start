4fd13a781a677fe849ace203c2c8effd3e5b4770
John Barson <jkbarson@gmail.com>
2018-06-06 12:59:12 -0400

    create shared module

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -8,6 +8,7 @@ import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { ProductGuardService } from './product-guard.service';
 import { ProductService } from './product.service';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   imports: [
@@ -18,7 +19,8 @@ import { ProductService } from './product.service';
       { path: 'products/:id', 
       canActivate: [ ProductGuardService ],
       component: ProductDetailComponent}
-    ])
+    ]),
+    SharedModule
   ],
   declarations: [
     ProductListComponent,
